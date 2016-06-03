b42bb48607421901ad5416ae36326ed8c85b4b6c
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2016-06-03 11:21:55 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const electron = require('electron');
+const keys = require(__dirname + 'keys.json');
+
+const {app} = electron;
+
+console.log('checking to see if we managed to load =' + keys.region)
+
+
